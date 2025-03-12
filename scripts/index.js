1e99c2470ae9c838426b882a77c07a0d327700c3
HabiburRahmanZihad <e241024@ugrad.iiuc.ac.bd>
2025-03-13 01:03:34 +0600

    primary work done also work in progress

--- scripts/index.js
+++ scripts/index.js
@@ -17,12 +17,13 @@ function loadCategories() {
         .then((data) => displayCategories(data.categories));
 }
 
-function loadVideos() {
-    fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+function loadVideos(searchText = '') {
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
         .then((res) => res.json())
         .then((data) => {
             removeActiveClass();
             document.getElementById('btn-all').classList.add('active');
+
             displayVideos(data.videos)
 
         });
@@ -39,14 +40,43 @@ function loadCategoriesVideos(id) {
 
 
             const clickedButton = document.getElementById(`btn-${id}`);
-            removeActiveClass();
 
+            removeActiveClass();
             clickedButton.classList.add('active');
 
             // console.log(clickedButton);
 
             displayVideos(data.category)
         });
+}
+
+const loadVideoDetails = (videoId) => {
+    // console.log(videoId);
+    const url = `https://openapi.programming-hero.com/api/phero-tube/video/${videoId}`;
+    // console.log(url);
+    fetch(url)
+        .then((res) => res.json())
+        .then(data => {
+            displayVideoDetails(data.video);
+        });
+}
+
+
+const displayVideoDetails = (video) => {
+    // console.log(video);
+    document.getElementById('video_details').showModal();
+    const detailsContainer = document.getElementById('details-container');
+    detailsContainer.innerHTML = `
+    <div class="card bg-base-100 image-full shadow-sm">
+        <figure>
+            <img src="${video.thumbnail}" alt="Shoes" />
+        </figure>
+        <div class="card-body">
+            <h2 class="card-title">${video.title}</h2>
+            <p>${video.description}</p>        
+        </div>
+    </div>
+    `;
 }
 
 /**
@@ -130,7 +160,7 @@ const displayVideos = (videos) => {
         //?create element
         const videoCard = document.createElement('div');
         videoCard.innerHTML = `
-        <div class="card bg-base-100">
+                    <div class="card bg-base-100">
 
                 <!--? Image -->
                 <figure class="relative">
@@ -147,8 +177,7 @@ const displayVideos = (videos) => {
                         <div class="avatar">
                             <div class="ring-primary ring-offset-base-100 w-8 rounded-full ring ring-offset-2">
 
-                                <img src="${video.authors[0].profile_picture}"
-                                    alt="Authors Profile Picture" />
+                                <img src="${video.authors[0].profile_picture}" alt="Authors Profile Picture" />
 
                             </div>
                         </div>
@@ -157,22 +186,33 @@ const displayVideos = (videos) => {
                     <!--? Profile Details -->
                     <div class="intro space-y-2">
                         <h2 class="text-base font-bold">${video.title}</h2>
-                        <p class="text-sm text-gray-400 flex gap-1 ">${video.authors[0].profile_name} 
-                        <img class="w-5 h-5 rounded-full"
-                                src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">
+                        <p class="text-sm text-gray-400 flex gap-1 ">${video.authors[0].profile_name}
+                            ${video.authors[0].verified ? `<img class="w-5 h-5 rounded-full"
+                                src="https://img.icons8.com/?size=48&id=98A4yZTt9abw&format=png" alt="">` : ''}
                         </p>
 
                         <p class="text-sm text-gray-400">${video.others.views} views</p>
                     </div>
 
+                    <div>
+                    </div>
                 </div>
-
+                <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
             </div>
-                `
+
+                `;
+
+
         //?append the element to the container
         vidContainer.appendChild(videoCard);
     });
 }
+
+
+document.getElementById('search-input').addEventListener('keyup', (e) => {
+    const input = e.target.value;
+    loadVideos(input);
+});
 
 //? call the function
 loadCategories();
